3815fd092733818e3eb2378f9f9fb0450f023d1a
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2023-07-13 19:53:39 +0100

    Fix host bug and add heathcheck for mysql

--- Connector/__MAIN__/config.py
+++ Connector/__MAIN__/config.py
@@ -1,11 +1,12 @@
+import os
 from dotenv import dotenv_values
 
 config: dict[str, str | None] = dotenv_values('.env')
 
 database_config: dict[str, str | int | None] = {
-    'host': config['DB_HOST'],
+    'host': os.environ['DB_HOST'] if os.environ['DB_HOST'] is not None else config['DB_HOST'],
     'user': config['DB_USERNAME'],
     'password': config['DB_PASSWORD'],
     'database': config['DB_DATABASE'],
-    'port': int(config['DB_PORT'] if config['DB_PORT'] != None else '0')
+    'port': int(os.environ['DB_HOST']) if os.environ['DB_HOST'] is not None else int(config['DB_PORT']) if config['DB_PORT'] is not None else None
 }
