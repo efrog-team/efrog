b54d3c1a082ba9633a300ac9bc585c9131a0d98c
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2023-07-14 10:12:41 +0100

    Update containers

--- Connector/__MAIN__/config.py
+++ Connector/__MAIN__/config.py
@@ -3,10 +3,21 @@ from dotenv import dotenv_values
 
 config: dict[str, str | None] = dotenv_values('.env')
 
+try:
+    host: str | None = os.environ['DB_HOST']
+except:
+    host: str | None = config['DB_HOST']
+
+try:
+    port: int | None = int(os.environ['DB_PORT']) if os.environ['DB_PORT'] is not None else None
+except:
+    port: int | None = int(config['DB_PORT']) if config['DB_PORT'] is not None else None
+
+
 database_config: dict[str, str | int | None] = {
-    'host': os.environ['DB_HOST'] if os.environ['DB_HOST'] is not None else config['DB_HOST'],
+    'host': host,
     'user': config['DB_USERNAME'],
     'password': config['DB_PASSWORD'],
     'database': config['DB_DATABASE'],
-    'port': int(os.environ['DB_PORT']) if os.environ['DB_PORT'] is not None else int(config['DB_PORT']) if config['DB_PORT'] is not None else None
+    'port': port
 }
